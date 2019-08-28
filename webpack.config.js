f59aedbc69670ecb114323457027cd5073a70c7d
Arky <hitmanarky@gmail.com>
2019-08-28 19:51:55 +0700

    Add node_modules exclude

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     rules: [{
       test: /\.js$/,
       include: path.resolve(__dirname, 'src'),
+      exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
         presets: ['react', 'env'],
