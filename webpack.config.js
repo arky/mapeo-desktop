77e72e80c0c0f1811cc09bf79a6d5ee655e3ed2a
Arky <hitmanarky@gmail.com>
2019-08-29 01:09:08 +0700

    Add sideEffects false to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,21 @@ const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: 'none',
+  optimization: {
+    sideEffects: false
+  },
   entry: './src/renderer/app.js',
   target: 'electron-renderer',
   externals: [nodeExternals({
     whitelist: ['lodash', 'randombytes', 'react-intl'],
-    modulesDir: path.resolve(__dirname, 'node_modules')
+    modulesDir: path.resolve(__dirname, 'node_modules'),
+    resolve:
+    {
+      'randombytes': 'randombytes',
+      // "./node_modules/randombytes/browser.js"
+      'react': 'react-intl'
+    }
+
   })],
   output: {
     filename: 'static/build.js',
@@ -22,6 +32,7 @@ module.exports = {
       query: {
         presets: ['react', 'env'],
         plugins: [
+          // new webpack.ProvidePlugin({join: ['lodash', 'join']})
           'transform-object-rest-spread',
           'transform-class-properties',
           'transform-es2015-classes'
@@ -29,4 +40,5 @@ module.exports = {
       }
     }]
   }
+
 }
